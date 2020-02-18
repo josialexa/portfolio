290318e69fb4390717f8d0b81415bdee301a45f7
Josi Reis-West <josialexa85@gmail.com>
2020-02-18 16:49:26 -0600

    Update project tech stacks

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -10,7 +10,8 @@ export default class Projects extends Component {
                         <p>The Hero Registry is a non-hosted 'No DB' project, based on the hit anime 'One Punch Man'
                             This project allows users to track hero statistics and view updated rankings based on events.
                         </p>
-                        <p>Github: <a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://github.com/josialexa/hero-registry'>hero-registry</a></p>
+                        <p><span className='bold'>Technologies: </span>Node, React, Express, Axios</p>
+                        <p><span className='bold'>Github: </span><a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://github.com/josialexa/hero-registry'>hero-registry</a></p>
                     </div>
                 </div>
                 <div className='project' id='anonet'>
@@ -19,8 +20,9 @@ export default class Projects extends Component {
                             Anonet is a hosted project, and relies on Socket.io to provide real time chat services to users on the site.
                             Users can create and join rooms, and chat with other members of those rooms.
                         </p>
-                        <p>Github: <a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://github.com/josialexa/anonet'>anonet</a></p>
-                        <p>Site link: <a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://anonet.xyz'>anonet.xyz</a></p>
+                        <p><span className='bold'>Technologies: </span>Node, React, Redux, Express, Axios, Socket.io, Bcrypt, Massive, PostGres</p>
+                        <p><span className='bold'>Github: </span><a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://github.com/josialexa/anonet'>anonet</a></p>
+                        <p><span className='bold'>Site link: </span><a className='projects-link' rel='noopener noreferrer' target='_blank' href='https://anonet.xyz'>anonet.xyz</a></p>
                     </div>
                 </div>
                 <div className='project' id='digital-couch'>
